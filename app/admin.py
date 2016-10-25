45d8f16b306c1b6cc49a25dcbfb9f229a5264cda
Kevin Velásquez <kevin.va0104@gmail.com>
2016-10-25 16:11:41 -0600

    cambio en get_image_path

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from app.models import Artist, Category, Note, Event, User
+
 # Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(Event)
+admin.site.register(Note)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,35 @@
 from django.db import models
+import os
 
 # Create your models here.
+def get_artist_image_path(instance, filename):
+    if instance.id is None:
+        return os.path.join('media/artists', str(1), filename)
+    else:
+        return os.path.join('media/artists', str(instance.id), filename)
 
-def get_image_path(instance, filename):
-    return os.path.join('products', str(instance.id), filename)
+def get_event_image_path(instance, filename):
+    if instance.id is None:
+        return os.path.join('media/events', str(1), filename)
+    else:
+        return os.path.join('media/events', str(instance.id), filename)
+
+def get_note_image_path(instance, filename):
+    if instance.id is None:
+        return os.path.join('media/notes', str(1), filename)
+    else:
+        return os.path.join('media/notes', str(instance.id), filename)
+
+def get_category_image_path(instance, filename):
+    if instance.id is None:
+        return os.path.join('media/categories', str(1), filename)
+    else:
+        return os.path.join('media/categories', str(instance.id), filename)
 
 class Category(models.Model):
-    name = models.CharField('Nombre de la categoría', max_length=45, null=False, blank=False)
-    description = models.TextField('Descripcion de la categoría', blank = True)
+    name = models.CharField('Nombre de la categoria', max_length=45, null=False, blank=False)
+    description = models.TextField('Descripcion de la categoria', blank = True)
+    image_profile = models.ImageField(upload_to=get_category_image_path, blank=True, null=True)
     def __str__(self):
         return self.name
 
@@ -19,18 +41,26 @@ class Artist(models.Model):
     phone_number = models.CharField('Numero de telefono', max_length=15, null=True, blank=True)
     email = models.EmailField(max_length=254, null=False, blank=False)
     address = models.CharField(max_length=150, null=True, blank=True)
-    image_profile = models.ImageField
+    image_profile = models.ImageField(upload_to=get_artist_image_path, blank=True, null=True)
     is_active = models.BooleanField(null=False, blank=False, default=True)
-    category = models.ForeignKey(Category, null=True, blank = False, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, null=True, blank = True, on_delete = models.CASCADE)
     def __str__(self):
         return self.nickname
 
+class Note(models.Model):
+    title = models.CharField(max_length=45, null=False, blank=False)
+    content = models.TextField()
+    category = models.ForeignKey(Category, null=False, blank = False, on_delete = models.CASCADE)
+    image_profile = models.ImageField(upload_to=get_note_image_path, blank=True, null=True)
+    def __str__(self):
+        return self.title
+
 class Event(models.Model):
     title = models.CharField('Titulo del evento', max_length=45, null=False, blank=False)
-    description = models.TextField('Descripción del evento')
+    description = models.TextField('Descripcion del evento')
     location = models.CharField('Lugar del evento', max_length=45, null=False, blank=False)
     date = models.DateTimeField('Fecha y hora')
-    #image event
+    image_profile = models.ImageField(upload_to=get_event_image_path, blank=True, null=True)
     Active = 'A'
     Finished = 'F'
     Canceled = 'C'
@@ -42,13 +72,6 @@ class Event(models.Model):
     state = models.CharField('Estado del evento', max_length=1, choices = state_choice, default = Active)
     categories = models.ManyToManyField(Category)
     arist = models.ManyToManyField(Artist)
-    def __str__(self):
-        return self.title
-
-class Note(models.Model):
-    title = models.CharField(max_length=45, null=False, blank=False)
-    content = models.TextField()
-    category = models.ForeignKey(Category, null=False, blank = False, on_delete = models.CASCADE)
     def __str__(self):
         return self.title
 
